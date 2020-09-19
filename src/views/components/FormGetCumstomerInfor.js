8b1fd3da2cb3efd6b9e51690862e8eaa6f87644a
tung9120 <vutung959595@gmail.com>
2020-09-19 22:03:45 +0700

    Fixed bug auto increment bill id

--- src/views/components/FormGetCumstomerInfor.js
+++ src/views/components/FormGetCumstomerInfor.js
@@ -29,7 +29,7 @@ class FormGetCumstomerInfor extends Component {
     const { cart, order, bills } = this.props;
     const { name, phone, adress } = values.user;
     const bill = {
-      billId: bills.length === 0 ? ("BILL" + 0) : ("BILL" + bills[bills.length - 1] + 1),
+      billId: bills.length === 0 ? ("BILL" + 0) : ("BILL" + (bills[bills.length - 1].key + 1)),
       key: bills.length === 0 ? 0 : bills[bills.length - 1] + 1,
       name,
       phone,
